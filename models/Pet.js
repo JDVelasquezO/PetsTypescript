fc72f76769594e466a2263e2cc2738e10dd2bd34
JDVelasquezO <jdvela158@gmail.com>
2019-11-27 16:38:37 -0600

    First upgrade

---
+++ models/Pet.js
@@ -0,0 +1,38 @@
+"use strict";
+var Pet = /** @class */ (function () {
+    function Pet(name, size, age, weigth) {
+        this._name = name;
+        this._size = size;
+        this._age = age;
+        this._weigth = weigth;
+    }
+    Object.defineProperty(Pet.prototype, "setName", {
+        set: function (v) {
+            this._name = v;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Pet.prototype, "setSize", {
+        set: function (v) {
+            this._size = v;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Pet.prototype, "setAge", {
+        set: function (v) {
+            this._age = v;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Pet.prototype, "setWeigth", {
+        set: function (v) {
+            this._weigth = v;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Pet;
+}());
---
+++ models/Pet.ts
@@ -0,0 +1,29 @@
+class Pet {
+  private _name: string;
+  private _size: number;
+  private _age: number;
+  private _weigth: number;
+
+  constructor(name: string, size: number, age: number, weigth: number) {
+    this._name = name;
+    this._size = size;
+    this._age = age;
+    this._weigth = weigth;
+  }
+
+  public set setName(v: string) {
+    this._name = v;
+  }
+
+  public set setSize(v: number) {
+    this._size = v;
+  }
+
+  public set setAge(v: number) {
+    this._age = v;
+  }
+
+  public set setWeigth(v: number) {
+    this._weigth = v;
+  }
+}
